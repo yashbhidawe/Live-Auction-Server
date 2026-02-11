22eabd43b2b967f78abce0b4e7c6a0e4b2db9741
Yash! <ybh5179@gmail.com>
2026-02-11 14:41:15 +0530

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOrigin = process.env.CORS_ORIGIN;
+  app.enableCors({
+    origin: corsOrigin ? corsOrigin.split(',') : true,
+    credentials: true,
+  });
   app.useWebSocketAdapter(new IoAdapter(app));
   await app.listen(process.env.PORT ?? 3000);
 }
